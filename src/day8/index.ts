08c2a86509312e41cdc4f0cfe37a0450757b1ba8
Joël Kema <joelkema1988@gmail.com>
2021-12-30 11:27:59 +0100

    Working day 8 part 2

--- src/day8/index.ts
+++ src/day8/index.ts
@@ -1,3 +1,4 @@
+import { countChars, difference, sortAlphabeticly, swap } from "../utils";
 import { input } from "./input";
 
 type Entry = {
@@ -5,17 +6,14 @@ type Entry = {
     outputValue: string[];
 };
 
-const segments: Record<string, number> = {
-    abcefg: 0,
-    cf: 1,
-    acdeg: 2,
-    acdfg: 3,
-    bcdf: 4,
-    abdfg: 5,
-    abdefg: 6,
-    acf: 7,
-    abcdefg: 8,
-};
+type Segment = "top" | "topLeft" | "topRight" | "center" | "bottomLeft" | "bottomRight" | "bottom";
+type Configuration = Record<string, Segment>;
+
+const two: Segment[] = ["top", "topRight", "center", "bottomLeft", "bottom"];
+const three: Segment[] = ["top", "topRight", "center", "bottomRight", "bottom"];
+const five: Segment[] = ["top", "topLeft", "center", "bottomRight", "bottom"];
+const six: Segment[] = ["top", "topLeft", "center", "bottomRight", "bottomLeft", "bottom"];
+const nine: Segment[] = ["top", "topLeft", "topRight", "center", "bottomRight", "bottom"];
 
 const entries = input.reduce((map: Entry[], row: string) => {
     const splittedRow = row.split(" | ");
@@ -27,19 +25,139 @@ const entries = input.reduce((map: Entry[], row: string) => {
     return map;
 }, []);
 
+const segmentsToNumber = (segment: Segment[]) => {
+    const hasEverySegment = (arr: Segment[]) => segment.every((s) => arr.includes(s));
+
+    if (segment.length === 2) return 1;
+    if (segment.length === 4) return 4;
+    if (segment.length === 3) return 7;
+    if (segment.length === 7) return 8;
+
+    if (hasEverySegment(two)) return 2;
+    if (hasEverySegment(three)) return 3;
+    if (hasEverySegment(five)) return 5;
+    if (hasEverySegment(six)) return 6;
+    if (hasEverySegment(nine)) return 9;
+
+    return 0;
+};
+
+const mapToNumber = (config: Configuration, pattern: string) => {
+    const chars = pattern.split("");
+    const segments = chars.map((char) => config[char]);
+    const number = segmentsToNumber(segments);
+
+    return number;
+};
+
+/**
+ * Given segment in: https://adventofcode.com/2021/day/8
+ *
+ * Each segment has the following occurences:
+ * top: 8 <-- not unique
+ * topLeft: 6
+ * topRight: 8
+ * center: 7
+ * bottomLeft: 4
+ * bottomRight: 9
+ * bottom: 7
+ */
+const getConfiguration = (signalPatterns: string[]) => {
+    const chars = countChars(signalPatterns.join(""));
+
+    const configuration: Configuration = {};
+
+    const allChars = Object.keys(chars);
+    const allSegments = Object.values(configuration);
+
+    // These are unique segments and can be added once
+    for (let char in chars) {
+        const amount = chars[char];
+
+        if (amount === 4) configuration[char] = "bottomLeft";
+        if (amount === 6) configuration[char] = "topLeft";
+        if (amount === 9) configuration[char] = "bottomRight";
+    }
+
+    const checkTop = !allSegments.includes("top");
+    const checkCenter = !allSegments.includes("center");
+    const checkBottom = !allSegments.includes("bottom");
+
+    while (Object.keys(configuration).length < 7) {
+        const usedChars = Object.keys(configuration);
+
+        if (checkTop) {
+            // Figure out top
+            // One and four don't have a top
+            const oneOrFour = signalPatterns
+                .filter(({ length }) => length === 2 || length === 4)
+                .join("")
+                .split("");
+
+            const possibleTopChars = allChars.filter((key) => !oneOrFour.includes(key) && !usedChars.includes(key));
+
+            if (possibleTopChars.length === 1) configuration[possibleTopChars[0]] = "top";
+        }
+
+        if (checkCenter) {
+            // Figure out center
+            // One and seven don't have a center
+            const oneOrSeven = signalPatterns
+                .filter(({ length }) => length === 2 || length === 3)
+                .join("")
+                .split("");
+
+            const possibleCenterChars = allChars.filter((key) => !oneOrSeven.includes(key) && !usedChars.includes(key));
+
+            if (possibleCenterChars.length === 1) configuration[possibleCenterChars[0]] = "center";
+        }
+
+        if (checkBottom) {
+            // Figure out bottom
+            const oneOrSevenOrFour = signalPatterns
+                .filter(({ length }) => length === 2 || length === 3 || length === 4)
+                .join("")
+                .split("");
+
+            const possibleBottomChars = allChars.filter(
+                (key) => !oneOrSevenOrFour.includes(key) && !usedChars.includes(key),
+            );
+
+            if (possibleBottomChars.length === 1) configuration[possibleBottomChars[0]] = "bottom";
+        }
+
+        // Figuring out topRight is difficult
+        if (usedChars.length === 6) {
+            const diff = difference(usedChars, allChars);
+
+            configuration[diff[0]] = "topRight";
+        }
+    }
+
+    return configuration;
+};
+
 // https://adventofcode.com/2021/day/8
-export const assignment1 = () =>
-    entries.flatMap((a) => a.outputValue).filter((x) => x.length < 5 || x.length === 7).length;
+export const assignment1 = () => entries.flatMap((a) => a.outputValue).filter((x) => x.length < 5 || x.length === 7);
 
+// https://adventofcode.com/2021/day/8#part2
 export const assignment2 = () => {
-    const map: Record<string, number> = {};
+    let total = 0;
 
-    // entries[0].signalPatterns.map((p) => {
-    //     map[p] = console.log("=======");
-    //     console.log(p);
-    //     console.log(sortAlphabetic(p));
-    //     console.log(segments[sortAlphabetic(p)]);
+    entries.forEach((entry) => {
+        const { signalPatterns, outputValue } = entry;
 
-    //     return 0;
-    // });
+        const config = getConfiguration(signalPatterns);
+
+        const digits = signalPatterns.reduce((map: Record<string, number>, pattern) => {
+            map[sortAlphabeticly(pattern)] = mapToNumber(config, pattern);
+            return map;
+        }, {});
+
+        const digit = Number(outputValue.map((v) => digits[sortAlphabeticly(v)]).join(""));
+
+        total += digit;
+    });
+
+    return total;
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,14 @@
 const add = (accumulator: number, a: number) => accumulator + a;
 
 export const sum = (array: number[]) => array.reduce(add, 0);
+
+export const difference = <T>(arr1: T[], arr2: T[]) =>
+    arr1.filter((x) => !arr2.includes(x)).concat(arr2.filter((x) => !arr1.includes(x)));
+
+export const swap = (obj: Record<any, any>): Record<any, any> =>
+    Object.assign({}, ...Object.entries(obj).map(([a, b]) => ({ [b]: a })));
+
+export const countChars = (s: string) =>
+    s.split("").reduce((res: Record<string, number>, char) => ((res[char] = (res[char] || 0) + 1), res), {});
+
+export const sortAlphabeticly = (s: string) => s.split("").sort().join("");
