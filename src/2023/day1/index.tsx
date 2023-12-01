71fbd3f0b0a846ef160b56b206b0b9a6e4a32b2a
Joël Kema <jkema@anwb.nl>
2023-12-01 15:30:21 +0100

    fixed part 2

--- src/2023/day1/index.tsx
+++ src/2023/day1/index.tsx
@@ -4,82 +4,45 @@ import { isNumber } from "../../shared/predicates";
 import { split } from "../../utils/string";
 import { input } from "./input";
 import { removeWhitespaces, sum } from "../../utils";
-import { map } from "../../utils/array";
 
-const tap = (a: any) => {
-    console.log(a);
-    return a;
+// this seems like a strange mapping,
+// however this makes it so that eightwo becomes e8ight2wo
+// which is easier to map to numbers
+// else we would get 8wo
+const numberTable: Record<string, string> = {
+    one: "o1ne",
+    two: "t2wo",
+    three: "t3hree",
+    four: "f4our",
+    five: "f5ive",
+    six: "s6ix",
+    seven: "s7even",
+    eight: "e8ight",
+    nine: "n9ine",
 };
 
-const numberTable: Record<string, number> = {
-    zero: 0,
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-};
-
-// 0 = e
-// 1 = i
-// 2 = g
-// 3 =
-
-// const slidingWindow = (array: string[], size: number) => {
-//     if (size > array.length) return null;
-
-//     const result = [];
-//     for (let i = 0; i < array.length - size + 1; i++) {
-//         result.push(array.slice(i, i + size));
-//     }
-
-//     return result;
-// };
-
-// slidingWindow("eightwonine".split(""), 5);
-
-// replace first word with a number
-// replace last word with a number
 // so eightwonine becomes 829
 const mapWordsToNumbers = (sentence: string) => {
-    sentence = "28gtbkszmrtmnineoneightmx";
-
-    const regex = new RegExp(/(?=(zero|one|two|three|four|five|six|seven|eight|nine))/, "g");
-
-    // positive lookahead assertion for overlapping matches
-
-    const matches = sentence.matchAll(regex);
-
-    debugger;
-
-    debugger;
     let output = sentence;
-    // if (matches.length === 0) return output;
-    // if (matches.length === 1) return output.replace(matches[0], numberTable[matches[0]].toString());
 
-    // console.log(matches);
-
-    // // replace first
-    // output = output.replace(matches[0], numberTable[matches[0]].toString());
-    // // replace last
-    // output = output.replace(matches[matches.length - 1], numberTable[matches[matches.length - 1]].toString());
+    Object.keys(numberTable).forEach((key) => {
+        output = output.replaceAll(key, numberTable[key]);
+    });
 
     return output;
 };
 
+const getLines = (i: string) => pipe(i, split(/\n/), A.map(removeWhitespaces));
+
 const assignment1 = () =>
     pipe(
-        input,
-        split(/\n/),
-        A.map(removeWhitespaces),
+        getLines(input),
         A.map((s: string) =>
             pipe(
                 s.split(""),
+                // map strings to numbers
                 A.map(Number),
+                // filter out NaN
                 A.filter(isNumber),
                 // get first and last number
                 (nums: number[]) => [nums[0], nums[nums.length - 1]],
@@ -92,24 +55,17 @@ const assignment1 = () =>
 
 const assignment2 = () =>
     pipe(
-        input,
-        split(/\n/),
-        A.map(removeWhitespaces),
+        getLines(input),
         A.map((s: string) =>
             pipe(
                 s,
-                tap,
                 mapWordsToNumbers,
-                // tap,
                 (s) => s.split(""),
                 A.map(Number),
                 A.filter(isNumber),
-                // tap,
                 // get first and last number
                 (nums: number[]) => [nums[0], nums[nums.length - 1]],
-                tap,
                 (nums: number[]) => nums.join(""),
-                // tap,
             ),
         ),
         A.map(Number),
