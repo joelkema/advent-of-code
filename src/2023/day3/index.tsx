d1bcfcba52379d7e0d41b87052615f163eb1447a
Joël Kema <jkema@anwb.nl>
2023-12-03 13:37:05 +0100

    add day3

---
+++ src/2023/day3/index.tsx
@@ -0,0 +1,17 @@
+const assignment1 = () => {
+    return 0;
+};
+
+const assignment2 = () => {
+    return 0;
+};
+
+const Day = () => (
+    <main>
+        <h2>Day 3</h2>
+        <p>Part one: {assignment1()}</p>
+        <p>Part two: {assignment2()}</p>
+    </main>
+);
+
+export default Day;
