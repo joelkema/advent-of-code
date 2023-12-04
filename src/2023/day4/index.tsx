51fb1dcfd58050874f78bc7c9f21e546d100154a
Joël Kema <jkema@anwb.nl>
2023-12-04 19:59:26 +0100

    little refactor for part2

--- src/2023/day4/index.tsx
+++ src/2023/day4/index.tsx
@@ -3,35 +3,72 @@ import { split } from "../../utils/string";
 import { removeWhitespaces, sum } from "../../utils";
 import * as A from "fp-ts/Array";
 import { input } from "./input";
-import { intersection, isNumber } from "lodash";
+import { intersection } from "fp-ts/Array";
+import * as N from "fp-ts/number";
+import * as O from "fp-ts/Option";
 
 const getLines = (i: string) => pipe(i, split(/\n/));
 
-const assignment1 = () => {
-    const cards = getLines(input).map((line) =>
-        line
-            .replace(/Card [1-9]:/, "")
-            .split(" | ")
-            .map((card) => card.split(" "))
-            .map((c) => c.filter((x) => !!x).map(Number)),
+const cards = getLines(input).map((line) =>
+    line
+        .replace(/Card [1-9]:/, "")
+        .split(" | ")
+        .map((card) => card.split(" "))
+        .map((c) => c.filter((x) => !!x).map(Number)),
+);
+
+const getSameNumbers = (c: number[][][]) =>
+    pipe(
+        c,
+        A.map((card) => {
+            const [winningNumbers, yourNumbers] = card;
+
+            return intersection(N.Eq)(winningNumbers, yourNumbers);
+        }),
     );
 
-    const points = cards.map((card) => {
-        const [winningNumbers, yourNumbers] = card;
-
-        const sameNumbers = intersection(winningNumbers, yourNumbers);
-
-        if (sameNumbers.length === 0) return 0;
-
-        console.log(sameNumbers.length);
-
-        return sameNumbers.length === 1 ? 1 : Math.pow(2, sameNumbers.length - 1);
-    });
-
-    return sum(points);
-};
+const assignment1 = () =>
+    pipe(
+        cards,
+        getSameNumbers,
+        A.map((sameNumbers) =>
+            pipe(
+                sameNumbers.length,
+                O.fromPredicate((length) => length > 0),
+                O.fold(
+                    () => 0,
+                    (length) => (length === 1 ? 1 : Math.pow(2, length - 1)),
+                ),
+            ),
+        ),
+        sum,
+    );
 
 const assignment2 = () => {
+    const winningCards: number[] = [];
+
+    const sameNumbers = pipe(cards, getSameNumbers);
+
+    for (let i = 0; i < sameNumbers.length; i++) {
+        const cardNumber = i + 1; // same as Card 1 etc.
+        const sameNumber = sameNumbers[i];
+
+        sameNumber.forEach((_, i) => {
+            winningCards.push(cardNumber + i);
+        });
+    }
+
+    for (let i = 0; i < winningCards.length; i++) {
+        const cardNumber = winningCards[i];
+
+        console.log(`Card ${cardNumber} is a winner!`);
+        console.log(sameNumbers[cardNumber - 1]);
+    }
+
+    debugger;
+
+    // while (iterator.length > 1) {}
+
     return 0;
 };
 
