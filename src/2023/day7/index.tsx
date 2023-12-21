14532e66dbb229c7d21ffbe21b142799d0f043a1
Joël Kema <jkema@anwb.nl>
2023-12-21 16:14:41 +0100

    start day 7

---
+++ src/2023/day7/index.tsx
@@ -0,0 +1,95 @@
+import { pipe } from "fp-ts/lib/function";
+import { split } from "../../utils/string";
+import { input } from "./input";
+
+import * as A from "fp-ts/Array";
+import { removeWhitespaces, sumProduct } from "../../utils";
+import { get } from "lodash";
+
+const getLines = (i: string) => pipe(i, split(/\n/));
+
+const cardStrength = {
+    A: 14,
+    K: 13,
+    Q: 12,
+    J: 11,
+    T: 10,
+    // rest is number
+};
+
+type Hand = {
+    cards: string[];
+    occurences: {
+        [key: string]: number;
+    };
+    strength: number;
+};
+
+const countUniqueChars = (s: string): Record<string, number> =>
+    s.split("").reduce((prev, curr) => {
+        prev[curr] = (prev[curr] || 0) + 1;
+        return prev;
+    }, {} as Record<string, number>);
+
+const getHand = (cards: string): Hand => {
+    const cardValues = cards.split("");
+    const occurences = countUniqueChars(cards);
+    const values = Object.values(occurences);
+
+    const hasFiveOfAKind = values.some((o) => o === 5);
+    const hasFourOfAKind = values.some((o) => o === 4);
+    const hasThreeOfAKind = values.some((o) => o === 3);
+    const hasTwoPair = values.filter((o) => o === 2).length === 2;
+    const hasOnePair = values.some((o) => o === 2);
+    const hasFullHouse = hasThreeOfAKind && hasOnePair;
+
+    const strength = hasFiveOfAKind
+        ? 6
+        : hasFourOfAKind
+        ? 5
+        : hasFullHouse
+        ? 4
+        : hasThreeOfAKind
+        ? 3
+        : hasTwoPair
+        ? 2
+        : hasOnePair
+        ? 1
+        : 0;
+
+    return {
+        cards: cardValues,
+        occurences,
+        strength,
+    };
+};
+
+const assignment1 = () => {
+    const lines = getLines(input);
+
+    const rounds = lines.map((s) => s.split(" "));
+
+    const hands = rounds
+        .map((round) => {
+            const cards = round[0];
+            const bid = Number(round[1]);
+
+            const hand = getHand(cards);
+
+            return hand;
+        })
+        .sort((a, b) => a.strength - b.strength);
+
+    debugger;
+};
+const assignment2 = () => {};
+
+const Day = () => (
+    <main>
+        <h2>Day 7</h2>
+        <p>Part one: {assignment1()}</p>
+        <p>Part two: {assignment2()}</p>
+    </main>
+);
+
+export default Day;
--- src/2023/index.ts
+++ src/2023/index.ts
@@ -4,5 +4,6 @@ import Day3 from "./day3";
 import Day4 from "./day4";
 import Day5 from "./day5";
 import Day6 from "./day6";
+import Day7 from "./day7";
 
-export default [Day1, Day2, Day3, Day4, Day5, Day6];
+export default [Day1, Day2, Day3, Day4, Day5, Day6, Day7];
