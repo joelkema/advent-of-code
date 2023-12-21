ada696ef1cc800e3d65392f02e1843795c9e02d4
Joël Kema <jkema@anwb.nl>
2023-12-21 14:58:58 +0100

    solved day6

---
+++ src/2023/day6/index.tsx
@@ -0,0 +1,77 @@
+import { pipe } from "fp-ts/lib/function";
+import { split } from "../../utils/string";
+import { input } from "./input";
+
+import * as A from "fp-ts/Array";
+import { removeWhitespaces, sumProduct } from "../../utils";
+import { get } from "lodash";
+
+type Race = {
+    lasts: number;
+    distance: number;
+};
+
+const getLines = (i: string) => pipe(i, split(/\n/));
+
+const parseLineWithSpaces = (line: string): number[] =>
+    line
+        .split(":")[1]
+        .split(" ")
+        .filter((s) => !!s)
+        .map(Number);
+
+const parseLineWithoutSpaces = (line: string) =>
+    pipe(
+        line,
+        (s) => s.split(":")[1],
+        removeWhitespaces,
+        (s) => Number(s),
+    );
+
+const distancePerMilliseconds = 1;
+
+const calculateWin = (race: Race) => {
+    // create array of all possible options
+    // start is 0 and end is equal to race.lasts
+    const options = Array.from(Array(race.lasts + 1).keys());
+
+    const traveled = options.map(
+        (buttonHoldingInMs) => (race.lasts - buttonHoldingInMs) * (buttonHoldingInMs * distancePerMilliseconds),
+    );
+
+    return traveled.filter((t) => t > race.distance);
+};
+
+const assignment1 = () =>
+    pipe(
+        getLines(input),
+        (lines) => ({
+            timeLine: parseLineWithSpaces(get(lines, 0)),
+            distanceLine: parseLineWithSpaces(get(lines, 1)),
+        }),
+        (obj) => obj.timeLine.map((time, i) => ({ lasts: time, distance: obj.distanceLine[i] })),
+        A.map(calculateWin),
+        A.map((w) => w.length),
+        sumProduct,
+    );
+
+const assignment2 = () =>
+    pipe(
+        getLines(input),
+        (lines) => ({
+            lasts: parseLineWithoutSpaces(get(lines, 0)),
+            distance: parseLineWithoutSpaces(get(lines, 1)),
+        }),
+        calculateWin,
+        A.size,
+    );
+
+const Day = () => (
+    <main>
+        <h2>Day 6</h2>
+        <p>Part one: {assignment1()}</p>
+        <p>Part two: {assignment2()}</p>
+    </main>
+);
+
+export default Day;
---
+++ src/2023/day6/input.ts
@@ -0,0 +1,2 @@
+export const input = `Time:        35     93     73     66
+Distance:   212   2060   1201   1044`;
