f26f09faa4723b4cfdc08430f7bf67011a1b53e0
Joël Kema <jkema@anwb.nl>
2024-02-04 17:02:35 +0100

    still not done with assignment2

--- src/2023/day8/index.tsx
+++ src/2023/day8/index.tsx
@@ -66,17 +66,50 @@ const assignment1 = () => {
     return steps;
 };
 
+const getZNodePath = (network: Network, instructions: ("L" | "R")[], nodeName: string) => {
+    let currentNodes = network[nodeName];
+    let steps = 0;
+    const maxInstructions = instructions.length;
+
+    for (let i = 0; i < 10000000; i++) {
+        const instruction = instructions[Math.floor(steps % maxInstructions)];
+        currentNodes = network[currentNodes[instruction]];
+        steps++;
+    }
+};
+
 const assignment2 = () => {
     const instructions = mapInputToInstructions(inputDay2);
     const network = mapInputToNetwork(inputDay2);
 
     // start at every node that ends with A, can be multiple
-    let currentNodes = Object.keys(network).filter((k) => k.endsWith("A"));
+    let currentNodes = ["FQA"];
     let steps = 0;
     const maxInstructions = instructions.length;
 
+    debugger;
+
+    //     0
+    // :
+    // "FQA"
+    // 1
+    // :
+    // "JSA"
+    // 2
+    // :
+    // "GJA"
+    // 3
+    // :
+    // "PBA"
+    // 4
+    // :
+    // "AAA"
+    // 5
+    // :
+    // "NNA"
+
     // follow all of the paths at the same time until they all simultaneously end up at nodes that end with Z
-    while (currentNodes.filter((k) => k.endsWith("Z")).length !== currentNodes.length) {
+    while (currentNodes.filter((k) => k.endsWith("Z")).length < 3) {
         // now with a for loop
         for (let i = 0; i < currentNodes.length; i++) {
             const currentNode = currentNodes[i];
@@ -86,6 +119,11 @@ const assignment2 = () => {
 
         steps++;
     }
+
+    console.log(currentNodes);
+
+    debugger;
+
     return steps;
 };
 
