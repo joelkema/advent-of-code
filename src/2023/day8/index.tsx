8f0a7ccf25ec6e6c18848570ec5b54ceeae3f599
Joël Kema <jkema@anwb.nl>
2024-02-04 16:34:23 +0100

    start with part 2, running very slow at the moment

--- src/2023/day8/index.tsx
+++ src/2023/day8/index.tsx
@@ -1,6 +1,6 @@
 import { pipe } from "fp-ts/lib/function";
 import { split } from "../../utils/string";
-import { input } from "./input";
+import { inputDay1, inputDay2 } from "./input";
 import * as S from "fp-ts/lib/string";
 import * as A from "fp-ts/lib/Array";
 import * as O from "fp-ts/lib/Option";
@@ -21,17 +21,9 @@ const getLines = (i: string) =>
         A.filter((s: string) => s !== ""),
     );
 
-const assignment1 = () => {
-    const lines = getLines(input);
-    const instructions = pipe(
-        lines,
-        A.head,
-        O.getOrElse(() => ""),
-        (s) => s.split("") as ("L" | "R")[],
-    );
-
-    const network = pipe(
-        lines,
+const mapInputToNetwork = (input: string): Network =>
+    pipe(
+        getLines(input),
         A.tail,
         O.getOrElse<string[]>(() => []),
         A.reduce({} as Network, (prev, curr: string) => {
@@ -47,12 +39,23 @@ const assignment1 = () => {
         }),
     );
 
+const mapInputToInstructions = (input: string): ("L" | "R")[] =>
+    pipe(
+        getLines(input),
+        A.head,
+        O.getOrElse(() => ""),
+        (s) => s.split("") as ("L" | "R")[],
+    );
+
+const assignment1 = () => {
+    const instructions = mapInputToInstructions(inputDay1);
+    const network = mapInputToNetwork(inputDay1);
+
     let currentNode = "AAA";
     let steps = 0;
     const maxInstructions = instructions.length;
 
     while (currentNode !== "ZZZ") {
-        // if maxInstructions is 3, by the 4th iteration we take the 0th instruction
         const instruction = instructions[Math.floor(steps % maxInstructions)];
 
         console.log(instruction);
@@ -64,14 +67,33 @@ const assignment1 = () => {
 };
 
 const assignment2 = () => {
-    return "Not implemented";
+    const instructions = mapInputToInstructions(inputDay2);
+    const network = mapInputToNetwork(inputDay2);
+
+    // start at every node that ends with A, can be multiple
+    let currentNodes = Object.keys(network).filter((k) => k.endsWith("A"));
+    let steps = 0;
+    const maxInstructions = instructions.length;
+
+    // follow all of the paths at the same time until they all simultaneously end up at nodes that end with Z
+    while (currentNodes.filter((k) => k.endsWith("Z")).length !== currentNodes.length) {
+        // now with a for loop
+        for (let i = 0; i < currentNodes.length; i++) {
+            const currentNode = currentNodes[i];
+            const instruction = instructions[Math.floor(steps % maxInstructions)];
+            currentNodes[i] = network[currentNode][instruction];
+        }
+
+        steps++;
+    }
+    return steps;
 };
 
 const Day = () => (
     <main>
         <h2>Day 8</h2>
         <p>Part one: {assignment1()}</p>
-        <p>Part two: {assignment2()}</p>
+        {/* <p>Part two: {assignment2()}</p> */}
     </main>
 );
 
