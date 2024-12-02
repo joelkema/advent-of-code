2269082e24767da7e3715cdf011b74c968843dd8
Joel <jkema@anwb.nl>
2024-12-02 16:36:55 +0100

    some extra prep

--- src/2024/day3/index.tsx
+++ src/2024/day3/index.tsx
@@ -1,24 +1,23 @@
-import { pipe } from "effect";
-import * as A from "effect/Array";
-import { split } from "../../utils/string";
+import { pipe, Array as A, String as S } from "effect";
 
-const getLines = (i: string) =>
-    pipe(
-        i,
-        split(/\n/),
-        A.filter((s) => !!s),
-        A.map(split(" ")),
-        A.map(A.map(Number)),
-    );
+const getLines = (i: string): number[][] =>
+	pipe(
+		i,
 
-const assignment1 = () => {};
+		S.split(/\n/),
+		A.filter((s) => !!s),
+		A.map(S.split(" ")),
+		A.map(A.map(Number)),
+	);
+
+const assignment1 = () => null;
 
 const Day = () => (
-    <main>
-        <h2>Day 2 </h2>
-        <p> Part one: {assignment1()} </p>
-        {/* <p> Part two: {assignment2()} </p> */}
-    </main>
+	<main>
+		<h2>Day 2 </h2>
+		<p> Part one: {assignment1()} </p>
+		{/* <p> Part two: {assignment2()} </p> */}
+	</main>
 );
 
 export default Day;
