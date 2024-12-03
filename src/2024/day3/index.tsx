0c04373735c1613a1fd844152b28515ac4ad8547
Joel <jkema@anwb.nl>
2024-12-03 13:12:30 +0100

    add day 3

--- src/2024/day3/index.tsx
+++ src/2024/day3/index.tsx
@@ -1,22 +1,36 @@
-import { pipe, Array as A, String as S } from "effect";
+import { input } from "./input";
 
-const getLines = (i: string): number[][] =>
-	pipe(
-		i,
-
-		S.split(/\n/),
-		A.filter((s) => !!s),
-		A.map(S.split(" ")),
-		A.map(A.map(Number)),
+const assignment1 = () =>
+	[...input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/gm)].reduce(
+		(acc: number, [, a, b]) => acc + Number(a) * Number(b),
+		0,
 	);
 
-const assignment1 = () => null;
+const assignment2 = () => {
+	let enabled = true;
+	let score = 0;
+
+	const matches = [...input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)/gm)];
+
+	for (const match of matches) {
+		const [op, a, b] = match;
+
+		if (op.startsWith("mul") && enabled) {
+			score += Number(a) * Number(b);
+		} else if (op === "do()") {
+			enabled = true;
+		} else if (op === "don't()") {
+			enabled = false;
+		}
+	}
+	return score;
+};
 
 const Day = () => (
 	<main>
-		<h2>Day 2 </h2>
+		<h2>Day 3</h2>
 		<p> Part one: {assignment1()} </p>
-		{/* <p> Part two: {assignment2()} </p> */}
+		<p> Part two: {assignment2()} </p>
 	</main>
 );
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root"),
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root!).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
 );
