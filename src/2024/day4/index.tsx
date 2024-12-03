f8ea2de59e68ed00e237b20514c4b7b7152ef758
Joel <jkema@anwb.nl>
2024-12-03 16:03:10 +0100

    add day4 prep

---
+++ src/2024/day4/index.tsx
@@ -0,0 +1,10 @@
+const assignment1 = () => null;
+
+const Day = () => (
+	<main>
+		<h2>Day 4</h2>
+		<p> Part one: {assignment1()} </p>
+	</main>
+);
+
+export default Day;
--- src/2024/index.ts
+++ src/2024/index.ts
@@ -1,5 +1,6 @@
 import Day1 from "./day1";
 import Day2 from "./day2";
 import Day3 from "./day3";
+import Day4 from "./day4";
 
-export default [Day1, Day2, Day3];
+export default [Day1, Day2, Day3, Day4];
