97cc953e101b4d6f0ec1c1ff1cd90c8b70d78ffd
Joel <jkema@anwb.nl>
2024-12-04 15:53:32 +0100

    add prep day 5

---
+++ src/2024/day5/index.tsx
@@ -0,0 +1,22 @@
+// import { pipe } from "effect";
+// import * as A from "effect/Array";
+// import { split } from "../../utils/string";
+//
+// const getLines = (i: string) =>
+// 	pipe(
+// 		i,
+// 		split(/\n/),
+// 		A.filter((s) => !!s),
+// 	);
+//
+const assignment1 = () => null;
+
+const Day = () => (
+	<main>
+		<h2>Day 5</h2>
+		<p> Part one: {assignment1()} </p>
+		{/* <p> Part two: {assignment2()} </p> */}
+	</main>
+);
+
+export default Day;
