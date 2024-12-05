1098f4986dffaf7db3c760be3e6d1fd70045bad5
Joel <jkema@anwb.nl>
2024-12-05 13:58:06 +0100

    add day6

---
+++ src/2024/day6/index.tsx
@@ -0,0 +1,24 @@
+// import { pipe } from "effect";
+// import * as A from "effect/Array";
+// import { split } from "../../utils/string";
+// import { input } from "./input";
+//
+// const getLines = (i: string) =>
+// 	pipe(
+// 		i,
+// 		split(/\n/),
+// 		A.filter((s) => !!s),
+// 		A.map(split(" ")),
+// 		A.map(A.map(Number)),
+// 	);
+//
+const assignment1 = () => null;
+
+const Day = () => (
+	<main>
+		<h2>Day 6 </h2>
+		<p> Part one: {assignment1()} </p>
+	</main>
+);
+
+export default Day;
--- src/2024/index.ts
+++ src/2024/index.ts
@@ -3,5 +3,6 @@ import Day2 from "./day2";
 import Day3 from "./day3";
 import Day4 from "./day4";
 import Day5 from "./day5";
+import Day6 from "./day6";
 
-export default [Day1, Day2, Day3, Day4, Day5];
+export default [Day1, Day2, Day3, Day4, Day5, Day6];
