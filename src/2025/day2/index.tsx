b797bc4d2c782c6cb261e7ad1c5337681a73cce9
Joel Kema <joelkema1988@gmail.com>
2025-12-01 11:30:29 +0100

    day2 init

---
+++ src/2025/day2/index.tsx
@@ -0,0 +1,15 @@
+const assignment1 = () => {
+	return 0;
+};
+
+// const assignment2 = () => { };
+
+const Day = () => (
+	<main>
+		<h2>Day 1 </h2>
+		<p> Part one: {assignment1()} </p>
+		{/* <p> Part two: {assignment2()} </p> */}
+	</main>
+);
+
+export default Day;
